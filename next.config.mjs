888716dae96d0edd0a482488692be0d33a664987
parikshitSJoshi <parikshit.s.joshi@gmail.com>
2025-03-29 20:44:16 +0530

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,8 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  output: 'export',
+  basePath: '/mithun-tagde',
 }
 
 mergeConfig(nextConfig, userConfig)
